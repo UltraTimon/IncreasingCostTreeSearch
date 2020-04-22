d55073f88d93425d6a19ca64d2b16bff6c2f4503
Timon Bestebreur <timonbestebreur@gmail.com>
2020-04-22 13:18:20 +0200

    Temporary working situation - quick push before laptop explodes

--- generateTree.cpp
+++ generateTree.cpp
@@ -8,22 +8,17 @@ Node generateRoot(int rootCost, int numberOfAgents) {
     }
     Node root = Node();
     root.data = rootData;
-    cout << "after generateRoot " << root.data.size() << endl;
     root.parent = nullptr;
     return root;
 }
 
 list<Node> generateNextLevelOfChildren(Node tree) {
-    cout << "hi from gen!" << endl;
     list<Node> children;
-    cout << "data length " << tree.data.size() << endl;
     for(int outer = 0; outer < tree.data.size(); ++outer) {
         list<int> childData;
         int inner = 0;
-        cout << "outer loop" << endl;
         for (auto parentDataIt = tree.data.begin(); parentDataIt != tree.data.end(); ++parentDataIt)
         {
-            cout << "outer " << outer << " inner " << inner << endl;
             if (outer == inner) {
                 childData.push_back(*parentDataIt + 1);
             }

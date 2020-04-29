e422b93fc91c38caa7601737dc9a14509d8df6cf
Timon Bestebreur <timonbestebreur@gmail.com>
2020-04-29 09:58:37 +0200

    Write plan for rewriting lowlevelsearch to work with the graph structure instead of with the path integer list

--- icts.cpp
+++ icts.cpp
@@ -8,33 +8,36 @@ void ICTS(vector<Agent> agentList, list<Node> queue) {
     cout << "Costs: ";
     printTree(&currentNode, 1);
 
-    vector<vector<vector<int>>> atLeastOnePathPerAgentWithoutConflict = getAtLeastOnePathPerAgentWithoutConflict(agentList, optimalCostList);
-    if (atLeastOnePathPerAgentWithoutConflict.size() > 0)
-    {
-        // announce the happy news
-        cout << "Ladies and Gentleman, we've got a solution!!" << endl;
-        for (auto pair : atLeastOnePathPerAgentWithoutConflict)
-        {
-            for (auto path : pair)
-            {
-                for (auto i : path)
-                {
-                    cout << i << " ";
-                }
-                cout << endl;
-            }
-            cout << endl;
-        }
-    } else {
-        // give error message
-        cout << "No paths found" << endl;
+    // BELOW WILL BE ADJUSTED LATER ON, AFTER REWRITING getAtLeastOnePathPerAgentWithoutConflict()
+    // ---------------------------------------------------------------------------------------------
 
-        // generate the next level of the tree, add those nodes to the queue and go to the next node
-        generateNextLevel(&currentNode);
-        for(auto child : currentNode.children) {
-            queue.push_back(child);
-        }
+    // vector<vector<vector<int>>> atLeastOnePathPerAgentWithoutConflict = getAtLeastOnePathPerAgentWithoutConflict(agentList, optimalCostList);
+    // if (atLeastOnePathPerAgentWithoutConflict.size() > 0)
+    // {
+    //     // announce the happy news
+    //     cout << "Ladies and Gentleman, we've got a solution!!" << endl;
+    //     for (auto pair : atLeastOnePathPerAgentWithoutConflict)
+    //     {
+    //         for (auto path : pair)
+    //         {
+    //             for (auto i : path)
+    //             {
+    //                 cout << i << " ";
+    //             }
+    //             cout << endl;
+    //         }
+    //         cout << endl;
+    //     }
+    // } else {
+    //     // give error message
+    //     cout << "No paths found" << endl;
 
-        ICTS(agentList, queue);
-    }
+    //     // generate the next level of the tree, add those nodes to the queue and go to the next node
+    //     generateNextLevel(&currentNode);
+    //     for(auto child : currentNode.children) {
+    //         queue.push_back(child);
+    //     }
+
+    //     ICTS(agentList, queue);
+    // }
 }
--- lowlevelsearch.cpp
+++ lowlevelsearch.cpp
@@ -32,20 +32,17 @@ bool pathsHaveConflict(vector<int> pathA, vector<int> pathB) {
     return false;
 }
 
-vector<vector<vector<int>>> getAtLeastOnePathPerAgentWithoutConflict(vector<Agent> agentList, vector<int> optimalCostList) {
-    // add the paths to the agent objects
-    for(auto it = agentList.begin(); it != agentList.end(); ++it) {
-        // auto it = agentList.begin();
-        auto costIt = optimalCostList.begin();
-        while(it != agentList.end() && costIt != optimalCostList.end()) {
-            // cout << "optimalCost index: " << *costIt << endl;
-            vector<vector<int>> generatedPaths;
-            generatePaths("resources/graph.txt", it->start, it->end, *costIt, &generatedPaths);
-            it->setPaths(generatedPaths);
-            ++it;
-            ++costIt;
-        }
-    }
+void getAtLeastOnePathPerAgentWithoutConflict(vector<Agent> agentList, vector<int> optimalCostList) {
+    // generate a useful graph for each agent
 
+    // conmbine those using the MDD magic from the paper
+
+    // check for conflicts by running modified nodeIsUseful method that marks nodes as useful based on whether or not they do or do not conflict
+    // we need a special graph structure for this, CombinedGraph or something like that
+
+    // if the combined source node has been marked as useful we know that there exists at least one path and we can return true
+    // then we can add the paths to a given pointer that holds the path data (which is stored in ICTS() or even main()), and add it just like in getPathsFromGraph()
+
+    // And then we're done... Sounds good, right?
 }
 
